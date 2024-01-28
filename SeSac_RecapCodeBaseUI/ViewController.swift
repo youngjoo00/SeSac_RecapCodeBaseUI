83246f9dafdf00807775eecab78bc561505aea45
youngjoo00 <yungjoo0624@gmail.com>
2024-01-28 20:27:35 +0900

    add: 초기 세팅

---
+++ SeSac_RecapCodeBaseUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSac_RecapCodeBaseUI
+//
+//  Created by youngjoo on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
